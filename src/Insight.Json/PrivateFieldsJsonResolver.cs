d834ce35274e8b3bb6871bd88c4737081920e686
Sergey Nazarov <insight.appdev@gmail.com>
2020-09-29 22:24:22 +0300

    Add Insight.Json

---
+++ src/Insight.Json/PrivateFieldsJsonResolver.cs
@@ -0,0 +1,25 @@
+﻿using System.Reflection;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+namespace Insight.Json
+{
+	public sealed class PrivateFieldsJsonResolver : CamelCasePropertyNamesContractResolver
+	{
+		protected override JsonProperty CreateProperty(MemberInfo member, MemberSerialization memberSerialization)
+		{
+			var property = base.CreateProperty(member, memberSerialization);
+
+			if (!property.Writable)
+			{
+				var info = member as PropertyInfo;
+				if (info != null)
+				{
+					property.Writable = info.GetSetMethod(true) != null;
+				}
+			}
+
+			return property;
+		}
+	}
+}

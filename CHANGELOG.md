0789d5e365a9e3a55a4fdc6e7314dcc84a998d72
Sergey Nazarov <insight.appdev@gmail.com>
2020-10-24 09:28:14 +0300

    Add Insight.Dto

---
+++ CHANGELOG.md
@@ -0,0 +1,1 @@
+1.0.5 - Add Insight.Dto project with custom error
---
+++ src/Insight.Dto/CustomError.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Insight.Dto
+{
+	public class CustomError
+	{
+		public string Message { get; private set; }
+
+		public Exception Exception { get; private set; }
+
+		public CustomError(string message)
+		{
+			Message = message;
+		}
+
+		public CustomError(string message, Exception ex) : this(message)
+		{
+			Exception = ex;
+		}
+
+		public static CustomError Create(string message)
+		{
+			return new CustomError(message);
+		}
+
+		public static CustomError Create(string message, Exception ex)
+		{
+			return new CustomError(message, ex);
+		}
+	}
+}

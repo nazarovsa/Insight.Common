df2171278d47c5b566b0c6b078c304751c192339
Sergey Nazarov <insight.appdev@gmail.com>
2020-12-05 13:37:16 +0300

    Add BackgroundServiceBase

---
+++ src/Insight.Hosting.Worker/Options/BackgroundServiceOptions.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Insight.Hosting.Worker.Options
+{
+	public sealed class BackgroundServiceOptions
+	{
+		public TimeSpan Interval { get; set; } = TimeSpan.FromSeconds(1);
+
+		public TimeSpan ExceptionDelay { get; set; } = TimeSpan.FromSeconds(5);
+	}
+}
---
+++ src/Insight.Hosting.Worker/Services/BackgroundServiceBase.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Insight.Hosting.Worker.Options;
+using Insight.Logging;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
+
+namespace Insight.Hosting.Worker.Services
+{
+	public abstract class BackgroundServiceBase : BackgroundService
+	{
+		private readonly BackgroundServiceOptions _options;
+		protected ILogService<BackgroundServiceBase> LogService { get; }
+
+		protected BackgroundServiceBase(ILogService<BackgroundServiceBase> logService,
+			IOptionsSnapshot<BackgroundServiceOptions> options)
+		{
+			LogService = logService ?? throw new ArgumentNullException(nameof(logService));
+			_options = options?.Value ?? throw new ArgumentNullException(nameof(options));
+		}
+
+		protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+		{
+			while (!stoppingToken.IsCancellationRequested)
+			{
+				try
+				{
+					await Task.WhenAll(UnitOfWork(stoppingToken),
+						Task.Delay(_options.Interval, stoppingToken));
+				}
+				catch (Exception ex)
+				{
+					LogService.Error(ex, "Error at background service");
+
+					await Task.Delay(_options.ExceptionDelay, stoppingToken);
+				}
+			}
+		}
+
+		protected abstract Task UnitOfWork(CancellationToken cancellationToken = default);
+	}
+}
